755cdbbaf9c9cc919fb3fc72fb5a565c2f8df17b
Deallia Dunbar <dealliadunbar@gmail.com>
2022-11-14 17:50:18 -0500

    Edited world.php

--- world.php
+++ world.php
@@ -1,7 +1,8 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -9,6 +10,28 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$country = strip_tags($_GET["country"]);
+
+if ($country!== NULL){
+  $answer = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $result = $answer->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $r){
+     echo "<table>";
+     echo "<tr>";
+     echo "<th> Name </th>";
+     echo "<th> Continent </th>";
+     echo "<th> Independence </th>";
+     echo "<th> Head of State </th>";
+     echo "</tr>";
+     echo "<tr>";
+     echo "<td>".$r['name']."</td>";
+     echo "<td>".$r['continent']."</td>";
+     echo "<td>".$r['independence_year']."</td>";
+     echo "<td>".$r['head_of_state']."</td>";
+     echo "</tr>";
+     echo "</table>";
+     }
+}
 ?>
 <ul>
 <?php foreach ($results as $row): ?>
