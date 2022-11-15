4ad11a221aab8e99a0dc21f28c0e3718f33d3242
Deallia Dunbar <dealliadunbar@gmail.com>
2022-11-14 19:50:54 -0500

    Edited world.js

--- world.js
+++ world.js
@@ -1,12 +1,13 @@
 window.onload = (event) => {
    
     
-    const button = document.querySelector("#lookup");
-    
-    const btnListener =() => button.addEventListener("click", handleClick);
-    
+    const buttonCo = document.querySelector("#lookup");
+    const buttonCi = document.querySelector("#cityLookup");
+
+    const btnListenerCo =() => buttonCo.addEventListener("click", handleClick1);
+    const btnListenerCi =() => buttonCi.addEventListener("click", handleClick2);
    
-    function handleClick(event){
+    function handleClick1(event){ //handle click for country
         var country =  document.querySelector("input").value;
       
         event.preventDefault();
@@ -17,13 +18,29 @@ window.onload = (event) => {
 
         var url= "world.php?country="+country;
         httpRequest.open('GET', url, true);
-        httpRequest.onreadystatechange = loadCountry;
+        httpRequest.onreadystatechange = loadPlace;
         httpRequest.send();
         console.log(country);
         
     };
+    function handleClick2(event){ // handle click for cities
+        var country =  document.querySelector("input").value;
+      
+        event.preventDefault();
+        
+    
+      httpRequest = new XMLHttpRequest();
 
-    function loadCountry() {
+
+        var url= "world.php?country="+country+"&Lookup=cities";
+        httpRequest.open('GET', url, true);
+        httpRequest.onreadystatechange = loadPlace;
+        httpRequest.send();
+    
+        
+    };
+
+    function loadPlace() {
         if (httpRequest.readyState == XMLHttpRequest.DONE) {
             if (httpRequest.status==200) {
                 var response = httpRequest.responseText;
@@ -36,5 +53,6 @@ window.onload = (event) => {
             }
         }
     }
-    btnListener();
+    btnListenerCo();
+    btnListenerCi();
 }
