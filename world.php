ef05f9a8aadb0614344ecf6cd5db16b1ce3fd9aa
Deallia Dunbar <dealliadunbar@gmail.com>
2022-11-14 19:51:12 -0500

    Edited world.php

--- world.php
+++ world.php
@@ -11,30 +11,59 @@ $stmt = $conn->query("SELECT * FROM countries");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $country = strip_tags($_GET["country"]);
+$cities="";
+if (isset($_GET["Lookup"]))
+$cities = strip_tags($_GET["Lookup"]);
 
-if ($country!== NULL){
-  $answer = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-  $result = $answer->fetchAll(PDO::FETCH_ASSOC);
-  foreach ($result as $r){
-     echo "<table>";
+if ($country!= NULL && $cities==Null){
+  $answer1 = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $result1 = $answer1->fetchAll(PDO::FETCH_ASSOC);
+  echo "<table>";
      echo "<tr>";
      echo "<th> Name </th>";
      echo "<th> Continent </th>";
      echo "<th> Independence </th>";
      echo "<th> Head of State </th>";
      echo "</tr>";
+  foreach ($result1 as $r1){
      echo "<tr>";
-     echo "<td>".$r['name']."</td>";
-     echo "<td>".$r['continent']."</td>";
-     echo "<td>".$r['independence_year']."</td>";
-     echo "<td>".$r['head_of_state']."</td>";
+     echo "<td>".$r1['name']."</td>";
+     echo "<td>".$r1['continent']."</td>";
+     echo "<td>".$r1['independence_year']."</td>";
+     echo "<td>".$r1['head_of_state']."</td>";
      echo "</tr>";
-     echo "</table>";
+     
      }
+     echo "</table>";
+}
+
+else if ($country!= NULL && $cities!= NULL){
+  $answer2 = $conn->query("SELECT c.name, c.district, c.population FROM cities c JOIN countries cs ON c.country_code  = cs.code And cs.name ='$country'");
+    
+  $result2 = $answer2->fetchAll(PDO::FETCH_ASSOC);
+  if ($result2==NULL)
+    echo "<h3> Enter the complete name of the country whose cities you want to look up.</h3>";
+  else {
+  echo "<table>";
+  echo "<tr>";
+  echo "<th> Name </th>";
+  echo "<th> District </th>";
+  echo "<th> Population </th>";
+  echo "</tr>";
+  foreach ($result2 as $r2){
+     echo "<tr>";
+     echo "<td>".$r2['name']."</td>";
+     echo "<td>".$r2['district']."</td>";
+     echo "<td>".$r2['population']."</td>";
+     echo "</tr>";
+     }
+     echo "</table>";
+  }
 }
 ?>
 <ul>
-<?php foreach ($results as $row): ?>
+<?php if ($country==NULL)
+    foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
